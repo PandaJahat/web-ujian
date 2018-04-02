6d10c4f41fc4c6d22aba4827f05982bcdc0015bc
Abdurrohim Saifi <buzzg@student.uns.ac.id>
2018-04-02 22:38:20 +0700

    Perbaikan judul formulir

--- resources/views/admin/exam/create.blade.php
+++ resources/views/admin/exam/create.blade.php
@@ -16,7 +16,7 @@ Penambahan Ujian
 @section('content')
   <div class="box">
     <div class="box-header with-border">
-      <h3 class="box-title">Formulir</h3>
+      <h3 class="box-title">Ujian Baru</h3>
       <div class="box-tools pull-right">
         <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
                 title="Collapse">
