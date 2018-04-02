4ea07e861ac7a6fb40ac0e6956fbd4a26a642cda
Abdurrohim Saifi <buzzg@student.uns.ac.id>
2018-04-02 23:09:07 +0700

    Penambahan file untuk memuat plugin

---
+++ resources/views/plugins/datatables.blade.php
@@ -0,0 +1,7 @@
+@push('links')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/plugin/datatables.css') }}">
+@endpush
+
+@push('scripts')
+<script type="text/javascript" charset="utf8" src="{{ asset('js/plugin/datatables.js') }}"></script>
+@endpush
---
+++ resources/views/plugins/icheck.blade.php
@@ -0,0 +1,7 @@
+@push('links')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/plugin/icheck/icheck.css') }}">
+@endpush
+
+@push('scripts')
+<script type="text/javascript" charset="utf8" src="{{ asset('js/plugin/icheck.js') }}"></script>
+@endpush
