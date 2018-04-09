ce4713c0076400c3f1d605f925507d0f77aeb2bb
Abdurrohim Saifi <buzzg@student.uns.ac.id>
2018-04-10 00:32:39 +0700

    Penmabahan Plugin: CKEDITOR

---
+++ resources/views/plugins/ckeditor.blade.php
@@ -0,0 +1,3 @@
+@push('scripts')
+    <script type="text/javascript" charset="utf8" src="{{ asset('js/plugin/ckeditor/ckeditor.js') }}"></script>
+@endpush
---
+++ resources/views/plugins/sweetalert2.blade.php
@@ -0,0 +1,3 @@
+@push('scripts')
+    <script src="https://unpkg.com/sweetalert2@7.1.3/dist/sweetalert2.all.js"></script>
+@endpush
