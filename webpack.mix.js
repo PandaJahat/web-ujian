c7c4a33640d6bd50be768a947c5aab1efd387272
Abdurrohim Saifi <buzzg@student.uns.ac.id>
2018-05-06 16:00:22 +0700

    Perubahan struktur folder bower

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,34 +2,34 @@ let mix = require('laravel-mix');
 
 // User CSS
 mix.styles([
-        'vendor/bower_components/bootstrap/dist/css/bootstrap.min.css',                
-        'vendor/bower_components/admin-lte/dist/css/AdminLTE.min.css',
-        'vendor/bower_components/admin-lte/dist/css/skins/_all-skins.min.css'
+        'bower_components/bootstrap/dist/css/bootstrap.min.css',                
+        'bower_components/admin-lte/dist/css/AdminLTE.min.css',
+        'bower_components/admin-lte/dist/css/skins/_all-skins.min.css'
 ], 'public/css/user/ujian.css');
 
 // User JS
 mix.scripts([
-        'vendor/bower_components/jquery/dist/jquery.min.js',
-        'vendor/bower_components/bootstrap/dist/js/bootstrap.min.js',
-        'vendor/bower_components/jquery-slimscroll/jquery.slimscroll.min.js',
-        'vendor/bower_components/fastclick/lib/fastclick.js',
-        'vendor/bower_components/admin-lte/dist/js/adminlte.min.js',
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'bower_components/jquery-slimscroll/jquery.slimscroll.min.js',
+        'bower_components/fastclick/lib/fastclick.js',
+        'bower_components/admin-lte/dist/js/adminlte.min.js',
 ], 'public/js/user/ujian.js');
 
 // Admin CSS
 mix.styles([
-        'vendor/bower_components/bootstrap/dist/css/bootstrap.min.css',                
-        'vendor/bower_components/admin-lte/dist/css/AdminLTE.min.css',
-        'vendor/bower_components/admin-lte/dist/css/skins/_all-skins.min.css'
+        'bower_components/bootstrap/dist/css/bootstrap.min.css',                
+        'bower_components/admin-lte/dist/css/AdminLTE.min.css',
+        'bower_components/admin-lte/dist/css/skins/_all-skins.min.css'
 ], 'public/css/admin/ujian.css');
 
 // Admin JS
 mix.scripts([
-        'vendor/bower_components/jquery/dist/jquery.min.js',
-        'vendor/bower_components/bootstrap/dist/js/bootstrap.min.js',
-        'vendor/bower_components/jquery-slimscroll/jquery.slimscroll.min.js',
-        'vendor/bower_components/fastclick/lib/fastclick.js',
-        'vendor/bower_components/admin-lte/dist/js/adminlte.min.js',
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'bower_components/jquery-slimscroll/jquery.slimscroll.min.js',
+        'bower_components/fastclick/lib/fastclick.js',
+        'bower_components/admin-lte/dist/js/adminlte.min.js',
 ], 'public/js/admin/ujian.js');
 
 // ------------------- Plug - in ----------------------------------------------
@@ -39,26 +39,26 @@ jsPath = 'public/js/plugin/';
 
 // DataTables CSS
 mix.styles([
-        'vendor/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css'        
+        'bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css'        
 ], cssPath + 'datatables.css');
 
 // DataTables JS
 mix.scripts([
-        'vendor/bower_components/datatables.net/js/jquery.dataTables.min.js',
-        'vendor/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js'
+        'bower_components/datatables.net/js/jquery.dataTables.min.js',
+        'bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js'
 ], jsPath + 'datatables.js');
 
 // ICheck CSS
 mix.styles([
-        'vendor/bower_components/admin-lte/plugins/iCheck/all.css'        
+        'bower_components/admin-lte/plugins/iCheck/all.css'        
 ], cssPath + 'icheck/icheck.css');
 
 // ICheck Plugin JS
 mix.scripts([
-        'vendor/bower_components/admin-lte/plugins/iCheck/icheck.min.js'        
+        'bower_components/admin-lte/plugins/iCheck/icheck.min.js'        
 ], jsPath + 'icheck.js');
 
 // CK-Editor Plugin JS
 mix.scripts([
-        'vendor/bower_components/admin-lte/bower_components/ckeditor/ckeditor.js'        
+        'bower_components/ckeditor/ckeditor.js'        
 ], jsPath + 'ckeditor/ckeditor.js');
